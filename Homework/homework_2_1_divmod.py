d7e32bbaac68e2a3799651d586e1232033e9adb4
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-04-20 22:16:56 +0300

    homework_2_1_added

--- Homework/homework_2_1_divmod.py
+++ Homework/homework_2_1_divmod.py
@@ -1,10 +1,10 @@
 user_input = input("Please enter a four-digit number:")
 num = int(user_input)
 div, mod = divmod(num, 100)
-a = div // 10
-b = div % 10
-c = mod // 10
-d = mod % 10
+a = div // 10  #  returns the first digit
+b = div % 10  # returns the second digit
+c = mod // 10  # returns the third digit
+d = mod % 10  # returns the fourth digit
 print(a)
 print(b)
 print(c)
