5b203b7c22b3e8dc85876ad20b876d5f4a30880d
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-04-20 22:16:56 +0300

    homework_2_1_added

--- Homework/homework_2_1.py
+++ Homework/homework_2_1.py
@@ -8,17 +8,3 @@ print(a)
 print(b)
 print(c)
 print(d)
-
-# variant with a divmod
-
-user_input = input("Please enter a four-digit number:")
-num = int(user_input)
-div, mod = divmod(num, 100)
-a = div // 10
-b = div % 10
-c = mod // 10
-d = mod % 10
-print(a)
-print(b)
-print(c)
-print(d)
---
+++ Homework/homework_2_2.py
@@ -0,0 +1,11 @@
+user_input = input("Please enter a five-digit number:")
+num = int(user_input)
+
+#  1) extract each digit using // and % 2) change the type to string to handle 0 digit
+a = str(num % 10)
+b = str(num // 10 % 10)
+c = str(num // 100 % 10)
+d = str(num // 1000 % 10)
+e = str(num // 10000)
+reversed_num = a + b + c + d + e
+print(reversed_num)
