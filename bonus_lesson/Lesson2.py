efeb16061abc6cf6f1eab7444fa2bd2e4e5be5dc
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-04-20 22:44:41 +0300

    test

---
+++ bonus_lesson/Lesson2.py
@@ -0,0 +1,4 @@
+my_guests = ['Maria Tereza', 'coco chanel', 'madonna', 'Sandra Bullock', 'princess diana', 'angelina jolie', 'Melissa MacCartnie']
+name = my_guests.pop()
+print(f"Hi, {name.title()}! Unfortunatelly, I can not invite you for dinner")
+print(my_guests)
