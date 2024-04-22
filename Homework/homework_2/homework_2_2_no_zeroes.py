b8adecf478e169bf65441fd9241be2a18886842e
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-04-22 14:26:08 +0300

    file deleted

--- Homework/homework_2/homework_2_2_no_zeroes.py
+++ Homework/homework_2/homework_2_2_no_zeroes.py
@@ -1,9 +1,11 @@
 user_input = input("Please enter a five-digit number:")
 num = int(user_input)
-a = str(num % 10)  # returns the fifth digit and turns it into a string
-b = str(num // 10 % 10)  # returns the fourth digit and turns it into a string
-c = str(num // 100 % 10)  # returns the third digit and turns it into a string
-d = str(num // 1000 % 10)  # returns the second digit and turns it into a string
-e = str(num // 10000)  # returns the first digit and turns it into a string
-reversed_number = a + b + c + d + e  # returns the sum of strings
-print(int(reversed_number))
+# a five-digit number can be written as follows: a * 10000 + b * 1000 + c * 100 + d * 10 + e.
+# To reverse a number, we will use the reverse digit order.
+a = (num % 10) * 10000  # returns the fifth digit and places it first
+b = (num // 10 % 10) * 1000  # returns the fourth digit and places it second
+c = (num // 100 % 10) * 100  # returns the third digit and places it third
+d = (num // 1000 % 10) * 10  # returns the second digit and places it fourth
+e = (num // 10000)  # returns the first digit and places it fifth
+reversed_number = a + b + c + d + e  # returns the reversed five-digit number
+print(reversed_number)
