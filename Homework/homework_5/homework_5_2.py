967cd1d5560b76777b0a32ec617951b2662a3094
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-05-01 19:02:42 +0300

    homework_5_2_changes

--- Homework/homework_5/homework_5_2.py
+++ Homework/homework_5/homework_5_2.py
@@ -1,9 +1,9 @@
 import string
 user_input = input("Please enter your text: ")
-no_punctuation_input = "".join(char for char in user_input.title() if char not in string.punctuation and char != " ")
-# capitalizes each word in user input + deletes the punctuation
-hashtag = "#"+no_punctuation_input  # adds # to the modified user input
+no_punctuation_input = "".join(char for char in user_input if char not in string.punctuation)  # deletes the punctuation
+no_spaces_input = no_punctuation_input.title().replace(" ", "")  # deletes spaces & capitalizes 1st words
+hashtag = "#" + no_spaces_input
 if len(hashtag) <= 140:  # if hashtag is <=140, display in full
     print(hashtag)
 else:
-    print(hashtag[:141])  # if hashtag >140, display the first 140 symbols
+    print(hashtag[:140])  # if not, display the first 140 symbols
