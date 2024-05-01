6915547f3e2496c36a96700807b7ac89149c656c
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-05-01 19:53:20 +0300

    homework_5_3_changes

--- Homework/homework_5/homework_5_3.py
+++ Homework/homework_5/homework_5_3.py
@@ -1,15 +1,20 @@
 import string
 import keyword
 variable_name = input("Please enter a variable name:")
-if variable_name[0].isdigit():  # checks if the first character is digit
+if variable_name == "_":
+    print("True")
+elif variable_name[0].isdigit():  # checks if the first character is digit
     print("False")
 elif any(character.isupper() for character in variable_name):  # checks if any character is in upper case
     print("False")
 elif any(character in string.punctuation and character != "_" or character == " " for character in variable_name):
     # character in punctuation/space excluding "_"
     print("False")
+elif all(character == "_" for character in variable_name):
+    print("False")
 elif variable_name in keyword.kwlist:  # checks if the variable is a keyword
     print("False")
 else:
     print("True")  # if not, print True
+
 
