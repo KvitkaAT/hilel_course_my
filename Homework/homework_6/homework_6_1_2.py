dd4e60b3ef21ec30ad5f20c8febe07af1a624ff1
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-05-04 17:04:26 +0300

    homework_6_1_2 changes

--- Homework/homework_6/homework_6_1_2.py
+++ Homework/homework_6/homework_6_1_2.py
@@ -1,5 +1,5 @@
 import string
-user_input = "a-c"
+user_input = input("Enter two letters divided by a hyphen: ")
 first_letter_index = string.ascii_letters.find(user_input[0])  # finds the index of the first letter
 last_letter_index = string.ascii_letters.find(user_input[-1])  # finds the index of the last letter
 print(string.ascii_letters[first_letter_index:last_letter_index + 1])  # displays all letters between the first & last
