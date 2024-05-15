7931632040f24c63048df5055f46552da187e9bc
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-05-15 14:16:26 +0300

    changes to 2

--- Homework/homework_9/homework_9_2.py
+++ Homework/homework_9/homework_9_2.py
@@ -1,12 +1,13 @@
 def difference(*args: int | float) -> int | float:
     if len(args) == 0:  # if no arguments, return 0
         return 0
-    return max(args) - min(args)  # calculates the difference between the max amd min argument
+    return round(max(args) - min(args), 2)  # calculates the difference between the max amd min argument
+#  rounds a result to 2 digits after the decimal point
 
 
 if __name__ == "__main__":
     assert difference(1, 2, 3) == 2
     assert difference(5, -5) == 10
-    assert round(difference(10.2, -2.2, 0, 1.1, 0.5), 2) == 12.4
+    assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4
     assert difference() == 0
     print('OK')
