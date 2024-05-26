f2da4d5789580105a01a056f38f90ce5a0901a5f
KvitkaAT <tymoshenko.anna.ta@gmail.com>
2024-05-26 12:38:12 +0300

    12_1 edits

--- Homework/homework_12/homework_12_1.py
+++ Homework/homework_12/homework_12_1.py
@@ -2,12 +2,12 @@ import codecs
 import re
 
 
-def delete_html_tags(html_file='draft.html', result_file='cleaned.txt'):
+def delete_html_tags(html_file="draft.html", result_file="cleaned.txt"):
     with codecs.open(html_file, "r", 'utf-8') as file:  # opens the file for reading
         html = file.read()
 
     cleaned_text = delete_html_tags_from_text(html)
-    with codecs.open(result_file, "w", 'utf-8') as file:  # opens the file for writing
+    with codecs.open(result_file, "w", "utf-8"g) as file:  # opens the file for writing
         file.write(cleaned_text)
 
 
@@ -18,5 +18,5 @@ def delete_html_tags_from_text(text: str) -> str:
 
 
 if __name__ == "__main__":
-    delete_html_tags('draft.html', 'cleaned.txt')
+    delete_html_tags("draft.html", "cleaned.txt")
     print("Ok")
